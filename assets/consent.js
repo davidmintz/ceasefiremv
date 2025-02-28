7c7d39830944e9e273a5d42d2d231365c0878074
david <david@davidmintz.org>
2025-02-28 16:48:43 -0500

    clean up consent.js debug statements

--- assets/consent.js
+++ assets/consent.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-console.log( `axios is a ${typeof axios}`   );
-console.log("Hello and WTF from consent.js");
+
+
 
 let button = document.getElementById("consent-button");
 button.addEventListener("click",
 function(){
-    console.log("shit is real in our 'click' event listener");
+    //console.log("shit is real in our 'click' event listener");
     axios.head('/privacy-consent')
         .then(function(response){
-            console.log(response)
-            button.setAttribute("hidden","hidden")
+            console.log(response);
+            button.setAttribute("hidden","hidden");
             document.getElementById("consent-thank-you").removeAttribute("hidden");
     });
 })
